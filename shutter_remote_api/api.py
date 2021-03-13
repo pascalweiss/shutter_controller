5df6128f0fe8b040fddbb94807b1cd8583008604
Pascal Weiß <pa-weiss@gmx.de>
2021-03-13 19:31:57 +0100

    use app logger

--- shutter_remote_api/api.py
+++ shutter_remote_api/api.py
@@ -6,13 +6,12 @@ from rf24_sender import RF24Sender
 
 app = Flask(__name__)
 sender = RF24Sender(22, 0, 0xF0F0F0F0E1, 5, 15)
-logger = logging.getLogger('shutter_remote_api')
 
 
 @app.route('/setShutterLevel', methods=['POST'])
 def set_shutter_level():
     level = request.json["level"]
-    logger.info("received level: {}".format(level))
+    app.logger.info("received level: {}".format(level))
     sender.send(level)
     return "New shutter level: {}".format(level)
 
