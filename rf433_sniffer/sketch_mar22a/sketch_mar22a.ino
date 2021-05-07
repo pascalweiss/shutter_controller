ddb48e297c5aade794526d78746926f717f33f81
Pascal Weiß <pa-weiss@gmx.de>
2021-05-07 16:02:06 +0200

    fixed formatting for sniffer

--- rf433_sniffer/sketch_mar22a/sketch_mar22a.ino
+++ rf433_sniffer/sketch_mar22a/sketch_mar22a.ino
@@ -1,25 +1,31 @@
 #include "RCSwitch.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 RCSwitch mySwitch = RCSwitch();
+
 void setup() {
- Serial.begin(9600);
- mySwitch.enableReceive(0);
+  Serial.begin(9600);
+  mySwitch.enableReceive(0); // this is actually D2
 }
-void loop() {
- if (mySwitch.available()) {
- int value = mySwitch.getReceivedValue();
- if (value == 0) {
- Serial.print("Unknown encoding");
- } else {
- Serial.print("Received ");
- Serial.print( mySwitch.getReceivedValue() );
- Serial.print(" / ");
- Serial.print( mySwitch.getReceivedBitlength() );
- Serial.print("bit ");
- Serial.print("Protocol: ");
- Serial.println( mySwitch.getReceivedProtocol() );
- }
- mySwitch.resetAvailable();
- }
+
+
+void loop() {  
+  if (mySwitch.available()) {
+    int value = mySwitch.getReceivedValue();
+    if (value == 0) {
+      Serial.print("Unknown encoding");
+    } else {
+      Serial.print("Received ");
+      Serial.print( mySwitch.getReceivedValue() );
+      Serial.print(" / ");
+      Serial.print( mySwitch.getReceivedBitlength() );
+      Serial.print("bit ");
+      Serial.print("Protocol: ");
+      Serial.println( mySwitch.getReceivedProtocol() );
+    }
+    
+    
+    mySwitch.resetAvailable();
+  }
 }
